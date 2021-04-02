57d0d8bc5f8576ecf5cfcdb242c2d431e19b2278
Kinan Dak Albab <kinan.bab.14@gmail.com>
2021-04-02 16:12:55 -0400

    More samples for testing name colisions

--- annotation-client/src/edu/brown/package-info.java
+++ annotation-client/src/edu/brown/package-info.java
@@ -2,8 +2,12 @@
     @JsonData(className = "Json1", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file1.json"),
     @JsonData(className = "Json2", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file2.json"),
     @JsonData(className = "Json3", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file3.json"),
-    @JsonData(className = "Json4", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file4.json")
-    // @JsonData(className = "Json5", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file5.json")
+    @JsonData(className = "Json4", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file4.json"),
+    @JsonData(className = "Json5", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file5.json"),
+    @JsonData(className = "Json6", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file6.json"),
+    @JsonData(className = "Json7", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file7.json"),
+    @JsonData(className = "Json8", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file8.json"),
+    @JsonData(className = "Json9", data = "/home/bab/Documents/courses/CSCI2950X/annotation-client/samples/file9.json"),
 })
 package edu.brown;
 
